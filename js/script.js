2613f7383180bf9ca41dca31b535ab09c3480027
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-13 00:31:24 +0200

    fix banner

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const bannerImgHtml = document.querySelector('.banner-image img');
 const bannerTxtHtml = document.querySelector('.banner-text h1');
 
 const images = ['../images/banner/2.jpg', '../images/banner/3.jpg', '../images/banner/4.jpg', '../images/banner/5.jpg', '../images/banner/6.jpg', '../images/banner/7.jpg'];
-const txt = ['bukieciki z lizaków', 'dekoracja aut', 'dekracja sal weselnych', 'wieńce pogrzebowe', 'stroiki świąteczne', 'ciekawe kompozycje'];
+const txt = [' słodkie bukiety', 'dekoracja aut', 'dekoracja sal weselnych', 'wieńce pogrzebowe', 'stroiki świąteczne', 'ciekawe kompozycje'];
 
 function changeElement() {
 	activeElement++;
@@ -24,3 +24,4 @@ function changeElement() {
 	bannerTxtHtml.textContent = txt[activeElement];	
 }
 setInterval(changeElement, timeChange)
+
