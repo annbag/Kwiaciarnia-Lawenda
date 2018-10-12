684ba0b419687bcb49ecd26c1db2628269428fa5
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-12 16:30:51 +0200

    update nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// nav
 const nav = document.querySelector('nav')
 document.querySelector('.burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	nav.classList.toggle('active');	
-}) 
+});
+
