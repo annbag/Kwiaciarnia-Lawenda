3728ef7d48a6a31ab65ab5763e76420cc7d07699
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-13 18:35:18 +0200

    test

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const bannerTxtHtml = document.querySelector('.banner-text h1');
 const arrowLeftHtml = document.querySelector('div.arrow-left i');
 const arrowRightHtml = document.querySelector('div.arrow-right i');
 
-const images = ['../images/banner/1.jpg', '../images/banner/2.jpg', '../images/banner/3.jpg', '../images/banner/4.jpg', '../images/banner/5.jpg', '../images/banner/6.jpg', '../images/banner/7.jpg'];
+const images = ['images/banner/1.jpg', '../images/banner/2.jpg', '../images/banner/3.jpg', '../images/banner/4.jpg', '../images/banner/5.jpg', '../images/banner/6.jpg', '../images/banner/7.jpg'];
 const txt = ['bukiety okolicznościowe 1', 'słodkie bukiety 2', 'dekoracja aut 3', 'dekoracja sal weselnych 4', 'wieńce pogrzebowe 5', 'stroiki świąteczne 6', 'ciekawe kompozycje 7'];
 
 //change slider
