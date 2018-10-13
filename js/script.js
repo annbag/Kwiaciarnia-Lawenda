1319687b6c91bb55f8562817ff2dd011bde61dbc
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-13 18:30:42 +0200

    add slider change after click arrow

--- js/script.js
+++ js/script.js
@@ -11,10 +11,13 @@ let timeChange = 4000;
 
 const bannerImgHtml = document.querySelector('.banner-image img');
 const bannerTxtHtml = document.querySelector('.banner-text h1');
+const arrowLeftHtml = document.querySelector('div.arrow-left i');
+const arrowRightHtml = document.querySelector('div.arrow-right i');
 
-const images = ['../images/banner/2.jpg', '../images/banner/3.jpg', '../images/banner/4.jpg', '../images/banner/5.jpg', '../images/banner/6.jpg', '../images/banner/7.jpg'];
-const txt = [' słodkie bukiety', 'dekoracja aut', 'dekoracja sal weselnych', 'wieńce pogrzebowe', 'stroiki świąteczne', 'ciekawe kompozycje'];
+const images = ['../images/banner/1.jpg', '../images/banner/2.jpg', '../images/banner/3.jpg', '../images/banner/4.jpg', '../images/banner/5.jpg', '../images/banner/6.jpg', '../images/banner/7.jpg'];
+const txt = ['bukiety okolicznościowe 1', 'słodkie bukiety 2', 'dekoracja aut 3', 'dekoracja sal weselnych 4', 'wieńce pogrzebowe 5', 'stroiki świąteczne 6', 'ciekawe kompozycje 7'];
 
+//change slider
 function changeElement() {
 	activeElement++;
 	if (activeElement == images.length) {
@@ -24,4 +27,28 @@ function changeElement() {
 	bannerTxtHtml.textContent = txt[activeElement];	
 }
 setInterval(changeElement, timeChange)
+
+arrowRightHtml.addEventListener('click', function() {
+	activeElement++;
+	if (activeElement == images.length) {
+		activeElement = 0;
+	} 
+	bannerImgHtml.src = images[activeElement];
+	bannerTxtHtml.textContent = txt[activeElement];
+	}
+) 
+
+arrowLeftHtml.addEventListener('click', function() {
+	activeElement--;
+	if (activeElement == images.length) {
+		activeElement = 0;
+	} 
+	bannerImgHtml.src = images[activeElement];
+	bannerTxtHtml.textContent = txt[activeElement];
+	// bannerImgHtml.src = images.reverse()[activeElement];
+	// bannerTxtHtml.textContent = txt.reverse()[activeElement];
+	}
+) 
+
+
 
