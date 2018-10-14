35892c1de9d09d92f53610aa34d26d1769720636
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-14 13:33:31 +0200

    add smaller image to banner

--- js/script.js
+++ js/script.js
@@ -10,11 +10,13 @@ let activeElement = 0;
 let timeChange = 4000;
 
 const bannerImgHtml = document.querySelector('.banner-image img');
+const bannerImgMiniHtml = document.querySelector('.banner-image-mini img')
 const bannerTxtHtml = document.querySelector('.banner-text h1');
 const arrowLeftHtml = document.querySelector('div.arrow-left i');
 const arrowRightHtml = document.querySelector('div.arrow-right i');
 
 const images = ['images/banner/1.jpg', 'images/banner/2.jpg', 'images/banner/3.jpg', 'images/banner/4.jpg', 'images/banner/5.jpg', 'images/banner/6.jpg', 'images/banner/7.jpg'];
+const imagesMini = ['images/banner/1mini.jpg', 'images/banner/2mini.jpg', 'images/banner/3mini.jpg', 'images/banner/4mini.jpg', 'images/banner/5mini.jpg', 'images/banner/6mini.jpg', 'images/banner/7mini.jpg'];
 const txt = ['bukiety okolicznościowe', 'słodkie bukiety', 'dekoracja aut', 'dekoracja sal weselnych', 'wieńce pogrzebowe', 'stroiki świąteczne', 'ciekawe kompozycje'];
 
 //change slider
@@ -23,7 +25,8 @@ function changeElement() {
 	if (activeElement == images.length) {
 		activeElement = 0;
 	}
-	bannerImgHtml.src = images[activeElement];	
+	bannerImgHtml.src = images[activeElement];
+	bannerImgMiniHtml.src = imagesMini[activeElement];	
 	bannerTxtHtml.textContent = txt[activeElement];	
 }
 setInterval(changeElement, timeChange)
@@ -34,6 +37,7 @@ arrowRightHtml.addEventListener('click', function() {
 		activeElement = 0;
 	} 
 	bannerImgHtml.src = images[activeElement];
+	bannerImgMiniHtml.src = imagesMini[activeElement];
 	bannerTxtHtml.textContent = txt[activeElement];
 	}
 ) 
@@ -44,9 +48,8 @@ arrowLeftHtml.addEventListener('click', function() {
 		activeElement = images.length-1;
 	} 
 	bannerImgHtml.src = images[activeElement];
+	bannerImgMiniHtml.src = imagesMini[activeElement];
 	bannerTxtHtml.textContent = txt[activeElement];
-	// bannerImgHtml.src = images.reverse()[activeElement];
-	// bannerTxtHtml.textContent = txt.reverse()[activeElement];
 	}
 ) 
 
