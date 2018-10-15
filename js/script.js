751c3aa30794de3b7147a4d8db7b20c4672e0a21
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-15 22:27:33 +0200

    add change color & scroll to button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ document.querySelector('.burger').addEventListener('click', function() {
 	nav.classList.toggle('active');	
 });
 //scroling menu
+
+	// const btn1 = document.querySelector('.btn-1');
+	// const btn2 = document.querySelector('.btn-2');
+	// const btn3 = document.querySelector('.btn-3');
+	// const btn4 = document.querySelector('.btn-4');
+	// const btn5 = document.querySelector('.btn-5');
+
+	// const beginningWelcome = document.querySelector('.welcome').offsetTop;
+	// const beginningAboutUs = document.querySelector('.about-us').offsetTop;
+	// const beginningBanner = document.querySelector('.banner').offsetTop;
+	// const beginningGallery = document.querySelector('.gallery').offsetTop;
+	// const beginningContact = document.querySelector('.contact').offsetTop;
+
+	// btn1.addEventListener('click', function() {
+	// 	beginningWelcome.scrollTop()
+	// 	})
+
+
+window.addEventListener('click', clickButton)
 $('.btn-1').on('click', function () {
 	$('body, html').animate({
 		scrollTop: $('.welcome').offset().top
@@ -12,19 +31,78 @@ $('.btn-1').on('click', function () {
 })
 $('.btn-2').on('click', function () {
 	$('body, html').animate({
-		scrollTop: $('.banner').offset().top
+		scrollTop: $('.about-us').offset().top
 	}, 500)
 })
 $('.btn-3').on('click', function () {
 	$('body, html').animate({
-		scrollTop: $('.gallery').offset().top
+		scrollTop: $('.banner').offset().top
 	}, 500)
 })
 $('.btn-4').on('click', function () {
 	$('body, html').animate({
-		scrollTop: $('.contact').offset().top
+		scrollTop: $('.gallery').offset().top
 	}, 500)
 })
+$('.btn-5').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.contact').offset().top
+	}, 500)
+})	
+
+//menu
+function changeColor() {
+	const sizeScroll = window.scrollY;
+
+	const btn1 = document.querySelector('.btn-1');
+	const btn2 = document.querySelector('.btn-2');
+	const btn3 = document.querySelector('.btn-3');
+	const btn4 = document.querySelector('.btn-4');
+	const btn5 = document.querySelector('.btn-5');
+
+	const heightWelcome = document.querySelector('.welcome').offsetHeight;
+	const heightAboutUs = document.querySelector('.about-us').clientHeight;
+	const heightBanner = document.querySelector('.banner').offsetHeight;
+	const heightGallery = document.querySelector('.gallery').clientHeight;
+	const heightContact = document.querySelector('.contact').clientHeight;
+
+	const beginningWelcome = document.querySelector('.welcome').offsetTop;
+	const beginningAboutUs = document.querySelector('.about-us').offsetTop;
+	const beginningBanner = document.querySelector('.banner').offsetTop;
+	const beginningGallery = document.querySelector('.gallery').offsetTop;
+	const beginningContact = document.querySelector('.contact').offsetTop;
+
+	if(sizeScroll < heightWelcome) {
+		btn1.style.color = "#c4bdff";
+		btn2.style.removeProperty('color');
+		btn3.style.removeProperty('color');
+		btn4.style.removeProperty('color');
+		btn5.style.removeProperty('color');
+	}
+	else if(sizeScroll < heightAboutUs + beginningAboutUs) {
+		btn2.style.color = "#957bff";
+		btn1.style.removeProperty('color');	
+		btn3.style.removeProperty('color');	
+	}
+	else if(sizeScroll < heightBanner + beginningBanner) {
+		btn3.style.color = "#7449ff";
+		btn2.style.removeProperty('color');	
+		btn4.style.removeProperty('color');	
+	}
+	else if(sizeScroll < heightGallery +beginningGallery) {
+		btn4.style.color = "#5127e5";
+		btn3.style.removeProperty('color');
+		btn5.style.removeProperty('color');
+		btn1.style.removeProperty('color');
+	}
+	else if(sizeScroll < heightContact + beginningContact) {
+		btn5.style.color = "#421eb2";
+		btn4.style.removeProperty('color');
+		btn1.style.removeProperty('color');
+	}
+}
+window.addEventListener('scroll', changeColor)
+
 // banner
 let activeElement = 0;
 let timeChange = 4000;
